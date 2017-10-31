667760f6a97529533e3d265819ce8203325bd07f
nickmvincent <nickmvincent@gmail.com>
2017-10-31 16:34:43 -0500

    still playing

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+"""performs scraping"""
 import serpscrap
 import pprint
 
-keywords = ['berlin']
+#VERSION = 'chrome'
+VERSION = 'phantom'
 
-config = serpscrap.Config()
-config.set('sel_browser', 'chrome')
-config.set('executable_path', 'C:\\Users\\Nick\\Desktop\\chromedriver.exe')
+def main():
+    """main driver"""
+    keywords = ['coffee near']
+    config = serpscrap.Config()
+    config.set('do_caching', False)
 
-scrap = serpscrap.SerpScrap()
-scrap.init(config=config.get(), keywords=keywords)
-results = scrap.as_csv('output')
+    if VERSION == 'chrome':
+        print('Using chromedriver.exe')
+        config.set('sel_browser', 'chrome')
+        config.set('executable_path', 'C:\\Users\\Nick\\Desktop\\chromedriver.exe')
+    else:
+        print('using phantomjs.exe')
+        config.set('executable_path', 'C:\\Users\\Nick\\Desktop\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe')
+
+    config.set('use_own_ip', False)
+    config.set('proxy_file', 'proxy.txt')
+
+    scrap = serpscrap.SerpScrap()
+    print('scrap.init')
+    scrap.init(config=config.get(), keywords=keywords)
+    results = scrap.as_csv('output')
+
+main()
