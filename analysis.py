628cb30254597a656a0bd5e8caa26cb98fa1996c
nickmvincent <nickmvincent@gmail.com>
2017-11-10 16:23:49 -0600

    various improvements

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import sqlite3
 from collections import defaultdict
 import operator
+import pandas as pd
 from sklearn.metrics import jaccard_similarity_score
 
 def dict_factory(cursor, row):
@@ -10,42 +11,47 @@ def dict_factory(cursor, row):
     return d
 
 
-DBNAME =  './tmp/chicago_neighborhoods2.db'
+DBNAME =  './tmp/cities.db'
 
 def main():
     conn = sqlite3.connect(DBNAME)
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    query_to_loc_to_pages = defaultdict(dict)
-    for zipcode in ['60653', '60660']:
-        domain_counter = defaultdict(int)
-        select_query = (
-            """
-            SELECT * from serp INNER JOIN link on serp.id = link.serp_id WHERE reported_location LIKE '{}%'
-            """
-        )
-        print('Here are rows in zip code %s' % zipcode)
-        for row in cur.execute(select_query.format(zipcode)):
-            if row['domain']:
-                domain_counter[row['domain']] += 1
-            if row['reported_location'] not in query_to_loc_to_pages[row['query']]:
-                query_to_loc_to_pages[row['query']][row['reported_location']] = []
-            query_to_loc_to_pages[row['query']][row['reported_location']].append(row)
-        sorted_domain_counter = sorted(
-            domain_counter.items(), key=operator.itemgetter(1), reverse=True)
-        print(sorted_domain_counter)
+    select_query = (
+        """
+        SELECT * from serp INNER JOIN link on serp.id = link.serp_id;
+        """
+    )
+    df = pd.read_sql_query(select_query, conn)
     conn.close()
-    for query, loc_to_pages in query_to_loc_to_pages.items():
-        links0 = [d['link'] for d in list(loc_to_pages.values())[0] if d['link_type'] == 'results' and d['link']]
-        links1 = [d['link'] for d in list(loc_to_pages.values())[1] if d['link_type'] == 'results' and d['link']]
-        print('For query {}, using jaccard to compare {} and {}'.format(query, *loc_to_pages.keys()))
-        try:            
-            jac = jaccard_similarity_score(links0, links1)
-            print(jac)
-            if jac != 1.0:
-                for item0, item1 in zip(links0, links1):
-                    print(item0, item1)
-        except:
-            print('mismatched number of results for the query "{}"'.format(query))
+    
+    df = df.fillna({
+        'isTweetCarousel': False,
+        'isMapsPlaces': False,
+        'isMapsLocations': False,
+        'isNewsCarousel': False,
+    })
+    df.domain = df.domain.astype('category')
+    location_set = df.reported_location.drop_duplicates()
+    print(df.describe())
+    print(df.domain.value_counts())
+    for reported_location in location_set:
+        filtered = df[df.reported_location == reported_location]
+        print(filtered.describe())
+        print(filtered.domain.value_counts())
+
+    
+    for serp_id in df[df.is_control == True].id:
+        filtered = df[df.id == serp_id]
+    # for query, loc_to_pages in query_to_loc_to_pages.items():
+    #     links0 = [d['link'] for d in list(loc_to_pages.values())[0] if d['link_type'] == 'results' and d['link']]
+    #     links1 = [d['link'] for d in list(loc_to_pages.values())[1] if d['link_type'] == 'results' and d['link']]
+    #     print('For query {}, using jaccard to compare {} and {}'.format(query, *loc_to_pages.keys()))
+    #     try:            
+    #         jac = jaccard_similarity_score(links0, links1)
+    #         print(jac)
+    #         if jac != 1.0:
+    #             for item0, item1 in zip(links0, links1):
+    #                 print(item0, item1)
+    #     except:
+    #         print('mismatched number of results for the query "{}"'.format(query))
 
 main()
--- main.py
+++ main.py
@@ -1,80 +1,32 @@
 """performs scraping"""
 import sys
-# Adds higher directory to python modules path.
-sys.path.append("C:/Users/Nick/Documents/GitHub/SerpScrap")
-import pandas as pd
-import serpscrap
-import time
 import json
+import pandas as pd
 from pytrends.request import TrendReq
 
-DBNAME = './tmp/chicago_neighborhoods3'
+sys.path.append("C:/Users/Nick/Documents/GitHub/SerpScrap")
+import serpscrap
+
+
+DBNAME = './tmp/cities'
 VERSION = 'chrome'
-LOCATIONS = [
-    # {},
-    # {
-    #     "city": "New York", 
-    #     "growth_from_2000_to_2013": "4.8%", 
-    #     "latitude": 40.7127837, 
-    #     "longitude": -74.0059413, 
-    #     "population": "8405837", 
-    #     "rank": "1", 
-    #     "state": "New York"
-    # },     {
-    #     "city": "New York", 
-    #     "growth_from_2000_to_2013": "4.8%", 
-    #     "latitude": 40.7127837, 
-    #     "longitude": -74.0059413, 
-    #     "population": "8405837", 
-    #     "rank": "1", 
-    #     "state": "New York"
-    # }, 
-    # {
-    #     "city": "Los Angeles", 
-    #     "growth_from_2000_to_2013": "4.8%", 
-    #     "latitude": 34.0522342, 
-    #     "longitude": -118.2436849, 
-    #     "population": "3884307", 
-    #     "rank": "2", 
-    #     "state": "California"
-    # },
-    # {
-    #     "city": "Jacksonville", 
-    #     "growth_from_2000_to_2013": "14.3%", 
-    #     "latitude": 30.3321838, 
-    #     "longitude": -81.65565099999999, 
-    #     "population": "842583", 
-    #     "rank": "13", 
-    #     "state": "Florida"
-    # },
-    # {
-    #     "city": "Wichita", 
-    #     "growth_from_2000_to_2013": "9.7%", 
-    #     "latitude": 37.688889, 
-    #     "longitude": -97.336111, 
-    #     "population": "386552", 
-    #     "rank": "49", 
-    #     "state": "Kansas"
-    # }, 
-]
+LOCATIONFILENAME = 'cities.json'
 
-with open('chicago_neighborhoods.json') as data_file:    
-    LOCATIONS = json.load(data_file)
+with open(LOCATIONFILENAME) as locationfile:
+    LOCATIONS = json.load(locationfile)
 
-NUM_LOCATIONS = 2
-NUM_KEYWORDS = 2
-
+NUM_LOCATIONS = 20
+NUM_KEYWORDS = 1
 
 
 def main():
     """main driver"""
-    # yearmonth = '201709'
-    # cid = 'fast_food_restaurants'
-    # pytrends = TrendReq(hl='en-US', tz=360)
-    # keywords = pytrends.top_charts(yearmonth, cid, geo='US')
-    # keywords = keywords.title.tolist()[:NUM_KEYWORDS]
-    # print(keywords)
-    keywords = ['coffee']
+    yearmonth = '201709'
+    cid = 'fast_food_restaurants'
+    pytrends = TrendReq(hl='en-US', tz=360)
+    keywords = pytrends.top_charts(yearmonth, cid, geo='US')
+    keywords = keywords.title.tolist()[:NUM_KEYWORDS]
+    print(keywords)
 
     config = serpscrap.Config()
     config.set('do_caching', False)
@@ -91,12 +43,12 @@ def main():
     # config.set('use_own_ip', False)
     # config.set('proxy_file', 'proxy.txt')
     config.set('num_pages_for_keyword', 1)
-    config.set('num_results_per_page', 30)
-    config.set('dir_screenshot', './tmp/screenshots')
+    config.set('num_results_per_page', 30)  # overshoots actual number of results per page
+    config.set('screenshot', False)
     config.set('database_name', DBNAME)
     for location in LOCATIONS:
         location['engine'] = 'google'
-    config.set('search_instances', LOCATIONS)    
+    config.set('search_instances', LOCATIONS[:NUM_LOCATIONS])
     scrap = serpscrap.SerpScrap()
     scrap.init(config=config.get(), keywords=keywords)
     results = scrap.run()
