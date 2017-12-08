810bc40946b4231eccf0404cb63e47912ede4f9f
nickmvincent <nickmvincent@gmail.com>
2017-12-07 22:49:25 -0600

    minor batching change

---
+++ batch.sh
@@ -0,0 +1,8 @@
+python main.py --num_locations 10 --query_source all --comparison urban-rural
+sleep 1h
+
+python main.py --num_locations 10 --query_source all --comparison income
+sleep 1h
+
+python main.py --num_locations 10 --query_source all --comparison voting
+sleep 1h
--- queue.sh
+++
@@ -1,8 +0,0 @@
-python main.python --num_locations 10 --query_source all --comparison urban-rural
-sleep 1h
-
-python main.python --num_locations 10 --query_source all --comparison income
-sleep 1h
-
-python main.python --num_locations 10 --query_source all --comparison voting
-sleep 1h
