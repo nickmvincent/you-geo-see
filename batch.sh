7b7f4f81565ef76a222bd5ef9c179ea49c49c9ce
nickmvincent <nickmvincent@gmail.com>
2018-01-01 17:58:37 -0600

    do pop weighted first

--- batch.sh
+++ batch.sh
@@ -1,3 +1,6 @@
+python main.py --num_locations 40 --query_source all --comparison population_weighted
+sleep 1h
+
 python main.py --num_locations 10 --query_source all --comparison income
 sleep 1h
 
@@ -5,7 +8,4 @@ python main.py --num_locations 10 --query_source all --comparison voting
 sleep 1h
 
 python main.py --num_locations 10 --query_source all --comparison urban-rural
-sleep 1h
-
-python main.py --num_locations 40 --query_source all --comparison population_weighted
 sleep 1h
